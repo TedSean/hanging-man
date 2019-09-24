7e65374eabe7afbacf3dca15c2a1f3fea23b4a3a
Chi Ho <>
2019-09-24 21:59:06 +0200

    api added

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+[
+    { 
+        "id": 1, 
+        "word": "bob" 
+    },
+    { 
+        "id": 2, 
+        "word": "marvin" 
+    },
+    { 
+        "id": 3, 
+        "word": "print" 
+    },
+    { 
+        "id": 4, 
+        "word": "filament" 
+    },
+    { 
+        "id": 5, 
+        "word": "order" 
+    },
+    { 
+        "id": 6, 
+        "word": "layer" 
+    }
+]
